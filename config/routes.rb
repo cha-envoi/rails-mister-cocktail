f6d6de6dc8562a968be876ebef0e3b14e72a6c83
Pauline Jaillot <paulinajaillot@gmail.com>
2020-06-12 14:06:11 +0200

    Add Article scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
  Rails.application.routes.draw do
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
+  # root to: "controller#action" pour la navbar
   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
   end
